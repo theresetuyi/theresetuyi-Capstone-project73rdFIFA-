f57029d20aefdf2b2ceecd17b7824e04393ed22d
Therese <teddytiyi@gmail.com>
2023-02-17 02:15:03 +0200

    add Images and style2.css

--- js/contect.js
+++ js/contect.js
@@ -23,8 +23,19 @@ const speakerArray = [
       title: 'President of the Confederation of African Football',
       description: ' Patrice Tlhopane Motsepe is a South African mining billionaire businessman. Since 12 March 2021, he has been serving as the President of the Confederation of African Football',
     },
-  ];
-
+    {
+      image: 'Images/Salman Ebrahim.PNG',
+      name: 'Salman bin Ibrahim Al Khalifa ',
+      title: 'president of the Asian Football Confederation',
+      description: ' the 10th President of the Asian Football Confederation with a resounding majority at the AFC Extraordinary Congress held in Kuala Lumpur, Malaysia, in May 2013. He was also elected to the FIFA Executive Committee (now known as the FIFA Council) by the same Congress.',
+      },
+      {
+      image: 'Images/Mugabo Olivier.png',
+      name: 'Mugabo Nizeyimana Olivier',
+      title: ' FERWAFA president',
+      description: 'Members of Rwanda Football Federation (FERWAFA) have backed businessman Olivier Nizeyimana to lead them in the next four years ',
+      },
+    ];
   const cardss = document.querySelector('.speaker-container');
   speakerArray.forEach((c) => {
     const card = document.createElement('article');
